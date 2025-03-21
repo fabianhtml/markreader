3187c79923479f7215bccfb1b02832d9d69345a9
fabianhtml <fabianhtml@gmail.com>
2025-03-21 10:39:28 -0300

    feat: Agregar soporte para selección de fuentes y modo oscuro, incluyendo nuevos componentes para la paleta de comandos, configuración de fuentes y alternancia de temas. Actualizar la configuración de Astro y el archivo package.json para incluir el plugin de sitemap. Añadir metadatos SEO y archivos de manifiesto y robots.txt para mejorar la indexación.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,16 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 import cloudflare from '@astrojs/cloudflare';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: cloudflare(),
+  site: 'https://markreader.pages.dev', 
+  integrations: [
+    sitemap(),
+  ],
   markdown: {
     syntaxHighlight: 'shiki',
     shikiConfig: {
